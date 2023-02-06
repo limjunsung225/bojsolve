c78c1d05238c401c5dde4da5ba1f1fe7a5ab6ce3
limjunsung225 <55376155+limjunsung225@users.noreply.github.com>
2023-02-06 15:04:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 264 ms, Memory: 47060 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,14 +1,11 @@
 import sys
-input =sys.stdin.readline
+input= sys.stdin.readline
 
 N = int(input())
 li = []
 for _ in range(N):
-    k = list(map(str,input().split()))
-    k[0] = int(k[0])
-    li.append(k)
-
-new_li = sorted(li, key=lambda x:(x[0]))
-
-for i in new_li:
+    age, name = map(str,input().rstrip().split())
+    li.append((int(age),name))
+li = sorted(li, key = lambda x:(x[0]))
+for i in li:
     print(*i)
