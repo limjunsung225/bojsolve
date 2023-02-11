cfecc3668eeadb5eb4f21ab23bc00d555ed4fceb
limjunsung225 <55376155+limjunsung225@users.noreply.github.com>
2023-02-12 00:34:24 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -6,14 +6,30 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		Queue<Integer> q = new LinkedList<Integer>();
-		for (int i=1; i<=N; i++) {
-			q.add(i);
+		
+		if (N==1) {
+			System.out.println(1);
+			System.exit(0);
 		}
-		while(q.size()!=1) {
-			q.remove();
-			q.add(q.poll());
+		
+		int cnt = 1;  
+		while (true) {  // 1->1  2->2 3->2 4->4 5->2 6->4 7->6 8->8 9->2 10->4 11->6 12->8
+			cnt *=2;
+			if(cnt>=N) {
+				cnt = cnt/2;
+				break;
+			}
 		}
-	System.out.println(q.poll());
+		System.out.println((N-cnt)*2);
+		
+//		Queue<Integer> q = new LinkedList<Integer>();
+//		for (int i=1; i<=N; i++) {
+//			q.add(i);
+//		}
+//		while(q.size()!=1) {
+//			q.remove();
+//			q.add(q.poll());
+//		}
+//	System.out.println(q.poll());
 	}
 }
