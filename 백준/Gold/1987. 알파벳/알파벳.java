6293d082c72d1788153905bcc9274ac94d308ec3
limjunsung225 <55376155+limjunsung225@users.noreply.github.com>
2023-02-21 14:23:46 +0900

    [Gold IV] Title: 알파벳, Time: 912 ms, Memory: 15684 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,46 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	  static int[] dx = {0, 0, 1, -1};
-	  static int[] dy = {1, -1, 0, 0};
-	  static int ans, next = 0;
+	  static int[] dx = {-1, 0, 0, 1};
+	  static int[] dy = {0, -1, 1, 0};
+	  static int R, C;
+	  static int max_result = 0;
 	  
-	  public static void dfs(String[] board, boolean[] check, int x, int y, int next) {
-	        if (ans < next) {
-	            ans = next;
-	        }
+	  public static void dfs(String[] graph, boolean[] check, int x, int y, int cnt) {
+		  
+	        if (cnt>max_result) max_result = cnt;
 	         
-	        for (int k=0; k<4; k++) {
-	            int nx = x+dx[k];
-	            int ny = y+dy[k];
-	            if (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length()) {
-	                if (check[board[nx].charAt(ny)-'A'] == false) {
-	                	
-	                    check[board[nx].charAt(ny)-'A'] = true;
-	                    dfs(board, check, nx, ny, next+1);
-	                    check[board[nx].charAt(ny)-'A'] = false;
-	                    
+	        for (int i=0; i<4; i++) {
+	            int nx = x+dx[i];
+	            int ny = y+dy[i];
+	            if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+	                if (check[graph[nx].charAt(ny)-'A'] == false) {
+	                    check[graph[nx].charAt(ny)-'A'] = true;
+	                    dfs(graph, check, nx, ny, cnt+1);
+	                    check[graph[nx].charAt(ny)-'A'] = false;
 	                }
 	            }
 	        }
 	    }
 	 
-	    public static void main(String args[]) {
-	        Scanner sc = new Scanner(System.in);
-	        int n = sc.nextInt();
-	        int m = sc.nextInt();
-	        sc.nextLine();
-	        String[] board = new String[n];
+	    public static void main(String args[]) throws IOException {
+	    	
+	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        StringBuilder sb = new StringBuilder();
 	        
-	        for (int i=0; i<n; i++) {
-	            board[i] = sc.nextLine();
+	        R = Integer.parseInt(st.nextToken());
+	        C = Integer.parseInt(st.nextToken());
+	        
+	        String[] graph = new String[R];
+	        
+	        for (int i=0; i<R; i++) {
+	            graph[i] = br.readLine();
 	        }
 	        
 	        boolean[] check = new boolean[26];
-	        check[board[0].charAt(0)-'A'] = true;
+	        check[graph[0].charAt(0)-'A'] = true;
 	        
-	        dfs(board, check, 0,0,1);
-	        System.out.println(ans);
+	        dfs(graph, check, 0,0,1); 
+	        
+	        sb.append(max_result);
+	        System.out.println(sb.toString());
+	        
 	    }
 	}
