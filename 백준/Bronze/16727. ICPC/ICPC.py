87d0930a21f98275c065f992c0e1907763b7fcd5
jsbryan <55376155+limjunsung225@users.noreply.github.com>
2023-05-11 21:59:02 +0900

    [Bronze IV] Title: ICPC, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/16727. ICPC/ICPC.py
@@ -0,0 +1,17 @@
+import sys
+input= sys.stdin.readline
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+n = a + d
+m = b + c
+if n == m:
+    if a == c:
+        print("Penalty")
+    elif a > c:
+        print("Esteghlal")        
+    else:
+        print("Persepolis")
+elif n > m:
+    print("Persepolis")
+else:
+    print("Esteghlal")
